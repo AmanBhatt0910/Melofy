3198ae48fff3c9bf6eb8958d425da19eac0d8a0b
Aman Bhatt <amanbhatt0910@gmail.com>
2025-05-19 15:37:59 +0530

    Latest Push

---
+++ melofy-backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Sazam App API is running');
+});
+
+app.use('/api', require('./routes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
